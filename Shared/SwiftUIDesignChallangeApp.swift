cce86e170a137a12bbd67324e14d64f70c65e5df
branch10480 <toshiharu.imaeda@gmail.com>
2022-03-21 18:18:21 +0900

    Initial Commit

---
+++ Shared/SwiftUIDesignChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDesignChallangeApp.swift
+//  Shared
+//
+//  Created by Toshiharu Imaeda on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDesignChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
